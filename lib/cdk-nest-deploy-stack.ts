9a86b6b955a30347293ccf6dfa91e4e67dfe385d
bivhunter <bivhunter@ukr.net>
2024-05-15 13:37:21 +0300

    init cdk

--- lib/cdk-nest-deploy-stack.ts
+++ lib/cdk-nest-deploy-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +8,46 @@ export class CdkNestDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkNestDeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const apiNestHandlerFunction = new Function(this, "ApiNestHandler", {
+      code: Code.fromAsset("api/dist"), // 👈 This is crucial
+      runtime: Runtime.NODEJS_18_X,
+      handler: "main.handler",
+      environment: {
+        STAGE: 'prod',
+        DB_HOST: '',
+        DB_PASSWORD: '',
+        DB_USERNAME: '',
+        DB_PORT: '',
+        DB_DATABASE: '',
+        JWT_SECRET: '',
+      }, // 👈 You might need env variables
+    });
+  
+    const api = new RestApi(this, "Api", {
+      deploy: true,
+      defaultMethodOptions: {
+      apiKeyRequired: true,
+      },
+  });
+  
+  api.root.addProxy({
+      defaultIntegration: new LambdaIntegration(apiNestHandlerFunction, { proxy: true }),
+  });
+  
+  const apiKey = api.addApiKey("ApiKey"); // 👈 to ease your testing
+  
+  const usagePlan = api.addUsagePlan("UsagePlan", {
+      name: "UsagePlan",
+      apiStages: [
+      {
+          api,
+          stage: api.deploymentStage,
+      },
+      ],
+  });
+  
+  usagePlan.addApiKey(apiKey);
+
   }
 }
