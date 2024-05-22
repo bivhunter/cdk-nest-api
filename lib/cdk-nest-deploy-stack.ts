e2412eacfd5bdc436693ff8461cd0e9c8753190a
bivhunter <bivhunter@ukr.net>
2024-05-22 10:46:16 +0300

    added FE host to allowOrigins

--- lib/cdk-nest-deploy-stack.ts
+++ lib/cdk-nest-deploy-stack.ts
@@ -5,54 +5,54 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkNestDeployStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
 
-    const apiNestHandlerFunction = new Function(this, "ApiNestHandler", {
-      code: Code.fromAsset("api/dist"), // 👈 This is crucial
-      runtime: Runtime.NODEJS_18_X,
-      handler: "main.handler",
-      environment: {
-        STAGE: 'prod',
-        DB_HOST: '',
-        DB_PASSWORD: '',
-        DB_USERNAME: '',
-        DB_PORT: '',
-        DB_DATABASE: '',
-        JWT_SECRET: '',
-      }, // 👈 You might need env variables
-    });
-  
-    const api = new RestApi(this, "Api", {
-      deploy: true,
-      defaultMethodOptions: {
-        apiKeyRequired: true,
-      },
-      defaultCorsPreflightOptions: {
-        allowOrigins: ["http://localhost:4200"],
-        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowHeaders: Cors.DEFAULT_HEADERS,
-      },
-  });
-  
-  api.root.addProxy({
-      defaultIntegration: new LambdaIntegration(apiNestHandlerFunction, { proxy: true }),
-  });
-  
-  const apiKey = api.addApiKey("ApiKey"); // 👈 to ease your testing
-  
-  const usagePlan = api.addUsagePlan("UsagePlan", {
-      name: "UsagePlan",
-      apiStages: [
-      {
-          api,
-          stage: api.deploymentStage,
-      },
-      ],
-  });
-  
-  usagePlan.addApiKey(apiKey);
+        const apiNestHandlerFunction = new Function(this, 'ApiNestHandler', {
+            code: Code.fromAsset('api/dist'), // 👈 This is crucial
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'main.handler',
+            environment: {
+                STAGE: 'prod',
+                DB_HOST: '',
+                DB_PASSWORD: '',
+                DB_USERNAME: '',
+                DB_PORT: '',
+                DB_DATABASE: '',
+                JWT_SECRET: '',
+            }, // 👈 You might need env variables
+        });
 
-  }
+        const api = new RestApi(this, 'Api', {
+            deploy: true,
+            defaultMethodOptions: {
+                apiKeyRequired: true,
+            },
+            defaultCorsPreflightOptions: {
+                allowOrigins: ['http://localhost:4200', 'https://bivhunter.github.io/'],
+                allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+                allowHeaders: Cors.DEFAULT_HEADERS,
+            },
+        });
+
+        api.root.addProxy({
+            defaultIntegration: new LambdaIntegration(apiNestHandlerFunction, { proxy: true }),
+        });
+
+        const apiKey = api.addApiKey('ApiKey'); // 👈 to ease your testing
+
+        const usagePlan = api.addUsagePlan('UsagePlan', {
+            name: 'UsagePlan',
+            apiStages: [
+                {
+                    api,
+                    stage: api.deploymentStage,
+                },
+            ],
+        });
+
+        usagePlan.addApiKey(apiKey);
+
+    }
 }
